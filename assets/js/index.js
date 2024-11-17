4cac3db35cd49c8c910bc813d11330a2c9e26297
Dylan Baker <dbaker5525@gmail.com>
2024-11-17 10:00:03 -0500

    Updating method of loading URL.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,15 @@
-function LoadWorld(baseWorldURL, urlID) {
-    nameInput = document.getElementById("user-name-" + urlID);
-    if (nameInput != null) {
-        window.location.href = baseWorldURL + "?USER_NAME=" + nameInput.value;
+for (i = 1; i < 7; i++) {
+    nameInput = document.getElementById("user-name-" + i);
+    loadLink = document.getElementById("load-link-" + i);
+
+    if (nameInput == null || loadLink == null) {
+        continue;
+    }
+
+    if (nameInput.value == null) {
+        loadLink.href = loadLink.href + "?USER_NAME=UNNAMED_USER";
+    }
+    else {
+        loadLink.href = loadLink.href + "?USER_NAME=" + nameInput.value;
     }
 }
