cc0a7319258b5b431d662af07f836a6095b97aef
Dylan Baker <dbaker5525@gmail.com>
2024-11-17 10:14:53 -0500

    Fixing URL assembly logic.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,25 @@
 for (i = 1; i < 7; i++) {
-    nameInput = document.getElementById("user-name-" + i);
-    loadLink = document.getElementById("load-link-" + i);
+    const nameInput = document.getElementById("user-name-" + i);
+    const loadLink = document.getElementById("load-link-" + i);
 
     if (nameInput == null || loadLink == null) {
         continue;
     }
 
-    if (nameInput.value == null) {
-        loadLink.href = loadLink.href + "?USER_NAME=UNNAMED_USER";
+    nameInput.addEventListener("input", function(evt) {console.log("sfdf " + loadLink.href);
+        if (loadLink.href.includes("?USER_NAME=")) {
+            loadLink.href = loadLink.href.substring(0, loadLink.href.indexOf("?USER_NAME="));
+        }
+        if (nameInput.value == null || nameInput.value == "") {
+            loadLink.href = loadLink.href + "?USER_NAME=NEW_USER";
+        }
+        else {
+            loadLink.href = loadLink.href + "?USER_NAME=" + nameInput.value;
+        }console.log(loadLink.href);
+    });
+
+    if (nameInput.value == null || nameInput.value == "") {
+        loadLink.href = loadLink.href + "?USER_NAME=NEW_USER";
     }
     else {
         loadLink.href = loadLink.href + "?USER_NAME=" + nameInput.value;
